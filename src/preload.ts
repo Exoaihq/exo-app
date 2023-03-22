f9ef1b77f5f9c53b6e7d8d9f2ac95b1e43b577db
sandeep1995 <i.am.sandeep.acharya@gmail.com>
2023-03-23 01:39:54 +0530

    fix: linting

--- src/preload.ts
+++ src/preload.ts
@@ -1,16 +1,16 @@
 // See the Electron documentation for details on how to use preload scripts:
 
-import { OpenAiResponseAndMetadata } from './api/apiCalls';
+import { OpenAiResponseAndMetadata } from "./api/apiCalls";
 
 // https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
 const { ipcRenderer, contextBridge } = require('electron');
 
 contextBridge.exposeInMainWorld('api', {
-  // Send Methods
-  testSend: (args: any) => ipcRenderer.send('test-send', args),
-  // Receive Methods
-  getBaseApiUrl: async () => await ipcRenderer.invoke('process'),
+    // Send Methods
+    testSend: (args: any) => ipcRenderer.send('test-send', args),
+    // Receive Methods
+    getBaseApiUrl: async () => await ipcRenderer.invoke('process'),
 
-  createOrUpdateFile: (response: OpenAiResponseAndMetadata) =>
-    ipcRenderer.invoke('create-or-update-file', response),
+    createOrUpdateFile: (response: OpenAiResponseAndMetadata) => ipcRenderer.invoke('create-or-update-file', response),
+
 });
