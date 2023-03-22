2c8c2550a1eab5f7b986b25b73e32a172678005c
sandeep1995 <i.am.sandeep.acharya@gmail.com>
2023-03-23 01:08:16 +0530

    feat: remove squirrel check

--- src/index.ts
+++ src/index.ts
@@ -23,11 +23,6 @@ const path = require("path");
 declare const MAIN_WINDOW_WEBPACK_ENTRY: string;
 declare const MAIN_WINDOW_PRELOAD_WEBPACK_ENTRY: string;
 
-// Handle creating/removing shortcuts on Windows when installing/uninstalling.
-if (require("electron-squirrel-startup")) {
-  app.quit();
-}
-
 export function createFile(fileName: string, text: string, folder = "./src") {
   const location = path.join(folder, fileName);
   fs.writeFile(location, text, (err: any) => {
